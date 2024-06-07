28d4a6cd09a7ace4ed6f19f31eb6bf16e2e03492
William-Wealsby <williamwealsby@gmail.com>
2024-06-07 13:57:17 +0000

    implemented basic fork structure for MP mothod

--- Trapezium/Trapezium.c
+++ Trapezium/Trapezium.c
@@ -1,6 +1,8 @@
 #include<stdio.h>
+#include<stdlib.h>
 #include<unistd.h>
 #include<sys/wait.h>
+#include<sys/types.h>
 
 typedef double(*fx)(double); 
 
@@ -21,13 +23,37 @@ double trapezium (double start, double end, int segments, fx fun){
     return total;
 }
 
+double trapezium_mp (double start, double end, int segments, fx fun, int processes){
+    double total = 0;
+    int status = 0;
+    double delta = (end-start)/(processes);
+    // use main process to manage forks, each fork runs
+    for (int i=0;i<processes;i++){
+        pid_t pid = fork();
+        if (pid<0){
+            printf("FORK ERROR");
+            exit(1);
+        }
+        if (pid==0){
+            printf("\n start %f:", start+i*delta);
+            printf("\n end %f:", start+(i+1)*delta);
+            total = trapezium(start+i*delta, start+(i+1)*delta, segments, fun);
+            printf("\n total = %f", total);
+            exit(0);
+        }
+    }
+    printf("\n mp: %f", total);
+    wait(&status);    
+    return total;
+}
+
 
 int main() {
     double total;
+    total = trapezium_mp(0,1,10,*f,3);
     total = trapezium(0,1,10,*f);
-    printf("integral = %f \n", total);
-
-
+    printf("\n trapezium: %f",total);
+    printf("\n");
 
 
     return 0;
