e760821014cc000a9beafc78d9ea576e87a04cdd
William-Wealsby <williamwealsby@gmail.com>
2024-06-07 15:18:14 +0000

    finished MultiProcess version of trapezium

--- Trapezium/Trapezium.c
+++ Trapezium/Trapezium.c
@@ -3,6 +3,7 @@
 #include<unistd.h>
 #include<sys/wait.h>
 #include<sys/types.h>
+#include<string.h>
 
 typedef double(*fx)(double); 
 
@@ -26,32 +27,42 @@ double trapezium (double start, double end, int segments, fx fun){
 double trapezium_mp (double start, double end, int segments, fx fun, int processes){
     double total = 0;
     int status = 0;
+    double buf;
     double delta = (end-start)/(processes);
+    int pipefd[processes][2];
+    segments /= processes;
     // use main process to manage forks, each fork runs
     for (int i=0;i<processes;i++){
+        pipe(pipefd[i]);
         pid_t pid = fork();
         if (pid<0){
             printf("FORK ERROR");
             exit(1);
         }
         if (pid==0){
-            total = trapezium(start+i*delta, start+(i+1)*delta, segments, fun);
-            printf("\n total = %f", total);
+            close(pipefd[i][0]);
+            buf = trapezium(start+i*delta, start+(i+1)*delta, segments, fun);
+            write(pipefd[i][1],&buf,sizeof(buf));
             exit(0);
         }
+        // close write end of pipe i for main process
+        close(pipefd[i][1]);    
     }
-    printf("\n mp: %f", total);
     wait(&status);    
+    for (int i=0;i<processes;i++){
+        read(pipefd[i][0],&buf,sizeof(buf));
+        total += buf;
+    }
     return total;
 }
 
 
 int main() {
     double total;
-    total = trapezium_mp(0,1,10,*f,3);
-    total = trapezium(0,1,10,*f);
-    printf("\n trapezium: %f",total);
-    printf("\n");
+    double total2;
+    total2 = trapezium_mp(0,1,100,*f,3);
+    total = trapezium(0,1,100,*f);
+    printf("trapezium: %f\n",total2);
 
 
     return 0;
